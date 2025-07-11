608032d3d4ba8b3752d1054be556addc3bb149f2
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 21:49:39 +0500

    create model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from books.models import Book
+from users.models import CustomUser
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, related_name='comments', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'book']
+
+    def __str__(self):
+        return f'{self.user.username} -> {self.book.title} - {self.rating}'
