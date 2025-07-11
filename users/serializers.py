e9f366b5c87612f36a25ecc5db856d6b84ccc562
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 09:56:47 +0500

    update serializer

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,7 @@ from users.models import CustomUser
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
