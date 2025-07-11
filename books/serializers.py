4d3dd311ed1a71c58f240bb98c152a7a5bc168a3
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 14:01:58 +0500

    create serializer

---
+++ books/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from books.models import Author, Book
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name', 'bio']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(write_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(
+        queryset=Author.objects.all(), source='author', write_only=True
+    )
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'author_id', 'price', 'created_at']
