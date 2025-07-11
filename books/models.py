57f355f24d2be7c7f490c99366873c3d5afc5320
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 14:01:41 +0500

    create model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
