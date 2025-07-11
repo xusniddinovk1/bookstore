01a6bb2e0a6aa4da4c36acc7fc66934838ee4eb0
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 09:53:35 +0500

    create user serializer

---
+++ users/serializers.py
@@ -0,0 +1,15 @@
+from users.models import CustomUser
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'phone_number', 'email', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser.objects.create_user(password=password, **validated_data)
+        return user
