03499a5fefe91480def386aa92d99bdd716e2f86
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 11:01:44 +0500

    update urls

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from users.views import LoginView, RegisterView
 
 urlpatterns = [
-    path('', RegisterView.as_view(), name='register'),
+    path('register', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login')
 ]
