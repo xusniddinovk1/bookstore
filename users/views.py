1a75c011b423ded1ac09470c506fdf4009dd46b3
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 11:06:37 +0500

    update views

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -7,6 +8,7 @@ from users.serializers import UserLoginSerializer, UserRegisterSerializer
 
 
 class RegisterView(APIView):
+    @swagger_auto_schema(request_body=UserRegisterSerializer)
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
@@ -21,6 +23,7 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+    @swagger_auto_schema(request_body=UserLoginSerializer)
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid():
