9343ab1f023f459fa44f40dfa4930ce3423f083c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-11 14:27:12 +0500

    update settings

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15
 }
 
 SIMPLE_JWT = {
