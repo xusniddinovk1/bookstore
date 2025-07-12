a6dea5b95eb16cf2fa98f8f0aac5b965504ff8aa
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-12 08:44:55 +0500

    create views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from comments.serializers import CommentSerializer
+from comments.models import Comment
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
