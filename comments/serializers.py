b64ed5a5c9c2db1c30e72c4bb6503c0ee320b20e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-12 08:41:17 +0500

    update serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,2 +1,11 @@
 from rest_framework import serializers
 from comments.models import Comment
+from users.models import CustomUser
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'book', 'text', 'rating', 'created_at']
