32fba2665e6a8e897a9cacc133c85d9fb7937956
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-13 21:41:32 +0500

    create views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from orders.permissions import IsOwnerOrReadOnly
+from .serializers import OrderSerializer
+from .models import Order
+from rest_framework import viewsets
 
-# Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IOwnerOrReadOnly]
