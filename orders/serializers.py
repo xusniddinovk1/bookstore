37561ccd99c66abad6587f8730915b5ca1b31c33
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-14 18:24:11 +0500

    update serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 from .models import Order
+from books.models import Book
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -14,6 +16,23 @@ class OrderSerializer(serializers.ModelSerializer):
     def get_total_price(self, obj):
         return obj.book.price * obj.amount
 
+    def validate_quantity(self, value):
+        try:
+            book_id = self.initial_data['book']
+            book = Book.objects.get(id=book_id)
+
+            if value > book.stock:
+                raise serializers.ValidationError('Not enough items in stock')
+            if value < 1:
+                raise serializers.ValidationError('Value must be at least 1')
+            return value
+        except ObjectDoesNotExist:
+            return serializers.ValidationError('Book does not exist')
+
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
+        order = Order.objects.create(**validated_data)
+        book = order.book
+        book.stock -= order.amount
+        book.save()
+        return order
