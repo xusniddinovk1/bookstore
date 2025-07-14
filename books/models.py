ea98b21661db356ca8eaf80f68886c793d114d4f
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-14 18:08:58 +0500

    update model

--- books/models.py
+++ books/models.py
@@ -9,12 +9,39 @@ class Author(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    def is_in_stock(self):
+        return self.stock > 0
+
+    def reduce_stock(self, quantity):
+        if self.stock < quantity:
+            return False
+
+        self.stock -= quantity
+        self.save()
+        return True
+
+    def increase_stock(self, amount):
+        self.stock += amount
+        self.save()
+        return True
+
+    class Meta:
+        ordering = ['title']
