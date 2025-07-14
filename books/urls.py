6a69bef64636b1a1e9197939cd4eca3836484d47
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-14 18:17:35 +0500

    update urls

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from books.views import AuthorViewSet, BookViewSet
+from books.views import AuthorViewSet, BookViewSet, CategoryViewSet
 from rest_framework.routers import DefaultRouter
 from orders.views import OrderViewSet
 from comments.views import CommentViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
+router.register(r'categories', CategoryViewSet)
 router.register(r'author', AuthorViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'orders', OrderViewSet)
