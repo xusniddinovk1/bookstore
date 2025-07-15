ba41492e8a7ed976ff9d11682a06b3855a83674f
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-15 15:21:25 +0500

    initial commit

--- books/filters.py
+++ books/filters.py
@@ -6,8 +6,7 @@ class BookFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
     category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
-    rating = django_filters.NumberFilter(field_name='rating', lookup_expr='gte')
 
     class Meta:
         model = Book
-        fields = ['category', 'max_price', 'min_price', 'rating']
+        fields = ['category', 'max_price', 'min_price']
