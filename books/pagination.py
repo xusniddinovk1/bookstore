353c3aa90fe8f8fe162fa379157aba0718d852fd
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-15 15:07:17 +0500

    update pagination

--- books/pagination.py
+++ books/pagination.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class Pagination(PageNumberPagination):
-    page_size = 5
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 50
