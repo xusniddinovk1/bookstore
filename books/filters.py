91afc489ad090f1993ec7ddcddf6b70b17c64a15
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-15 14:38:29 +0500

    create filters

---
+++ books/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as django_filters
+from books.models import Book
+
+
+class BookFilter(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
+    category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
+    rating = django_filters.NumberFilter(field_name='rating', lookup_expr='gte')
+
+    class Meta:
+        model = Book
+        fields = ['category', 'max_price', 'min_price', 'rating']
