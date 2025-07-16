91ad3597000a7798f319e90f7b0791fc138d3dce
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-16 10:38:30 +0500

    update views

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from orders.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from .serializers import OrderSerializer
 from .models import Order
 from rest_framework import viewsets
@@ -7,10 +7,10 @@ from rest_framework import viewsets
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+        return self.queryset.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        return serializer.save(user=self.request.user)
+        serializer.save(user=self.request.user)
