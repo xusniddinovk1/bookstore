929ba08ec36675e27baa6a3332c7479e772afbe2
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-18 09:54:44 +0500

    update serializers

--- books/serializers.py
+++ books/serializers.py
@@ -18,14 +18,23 @@ class CategorySerializer(serializers.ModelSerializer):
 class BookSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     avg_rating = serializers.SerializerMethodField()
+    discounted_price = serializers.SerializerMethodField()
+    flash_sale_active = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'category', 'price', 'avg_rating', 'stock', 'created_at']
+        fields = ['id', 'title', 'author', 'category', 'price', 'discounted_price',
+                  'flash_sale_active', 'avg_rating', 'stock', 'created_at']
 
     def get_avg_rating(self, obj):
         avg = obj.comments.aggregate(avg=Avg('rating'))['avg']
         return round(avg, 2) if avg else None
+
+    def get_discounted_price(self, obj):
+        return obj.get_discounted_price()
+
+    def get_flash_sale_active(self, obj):
+        return bool(obj.get_active_flash_sale())
 
 
 class BookCreateSerializer(serializers.ModelSerializer):
