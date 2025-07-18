cc35580ce59499f4d5d5be1b7254c99720c76153
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-18 10:14:40 +0500

    update filter

--- books/filters.py
+++ books/filters.py
@@ -1,5 +1,5 @@
 from django_filters import rest_framework as django_filters
-from books.models import Book, Author, Category
+from books.models import Book, Author, Category, FlashSale
 
 
 class BookFilter(django_filters.FilterSet):
@@ -22,3 +22,13 @@ class CategoryFilter(django_filters.FilterSet):
     class Meta:
         model = Category
         fields = ['name']
+
+
+class FlashSaleFilter(django_filters.FilterSet):
+    min_discount_percentage = django_filters.NumberFilter(field_name='discount_percentage', lookup_expr='gte')
+    max_discount_percentage = django_filters.NumberFilter(field_name='discount_percentage', lookup_expr='lte')
+    book = django_filters.CharFilter(field_name='book__title', lookup_expr='icontains')
+
+    class Meta:
+        model = FlashSale
+        fields = ['min_discount_percentage', 'max_discount_percentage', 'book']
