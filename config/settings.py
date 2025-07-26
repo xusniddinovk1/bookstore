d016e50189f03de3d25bbcf9720d0df35ad03f7e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-26 13:43:07 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -62,16 +62,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
