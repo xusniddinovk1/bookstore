a7225d37f8044fd77dd40cf3343423224740f763
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-26 14:08:10 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -64,14 +64,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
-
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-# }
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
